312209fa8985440c6f6595a2aadf6907eaa32e58
Chia-Wei Li <dreamcwli@viscovery.co>
2017-03-26 23:57:57 +0800

    Create input bar

---
+++ app/components/InputBar/index.js
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from 'react';
+import { Button, TextInput, View } from 'react-native';
+
+import styles from './styles';
+
+const propTypes = {
+  style: View.propTypes.style,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+const initialState = {
+  text: '',
+};
+
+class InputBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = initialState;
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleButtonPress = this.handleButtonPress.bind(this);
+  }
+
+  handleTextChange(text) {
+    this.setState({ text });
+  }
+
+  handleButtonPress() {
+    this.props.onSubmit && this.props.onSubmit(this.state.text);
+    this.setState(initialState);
+  }
+
+  render() {
+    return (
+      <View style={[styles.container, this.props.style]}>
+        <TextInput
+          placeholder="Todo"
+          style={styles.text}
+          value={this.state.text}
+          onChangeText={this.handleTextChange}
+        />
+        <Button
+          title="Submit"
+          disabled={!this.state.text}
+          onPress={this.handleButtonPress}
+        />
+      </View>
+    );
+  }
+}
+
+InputBar.propTypes = propTypes;
+
+export default InputBar;
