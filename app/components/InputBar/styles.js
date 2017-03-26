268ee961225ee501f66f9648304d9e0c6e16985b
Chia-Wei Li <dreamcwli@viscovery.co>
2017-03-26 23:57:57 +0800

    Create input bar

---
+++ app/components/InputBar/styles.js
@@ -0,0 +1,30 @@
+import { Platform, StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: '#fff',
+    ...Platform.select({
+      android: {
+        height: 56,
+        elevation: 2,
+      },
+      ios: {
+        paddingTop: 8,
+        paddingBottom: 8,
+      },
+    }),
+  },
+  text: {
+    flex: 1,
+    marginRight: 16,
+    ...Platform.select({
+      android: {
+        fontSize: 16,
+      },
+    }),
+  },
+});
